fabe72ecd0ec4a61e428caeec0681d7395a1a791
5aura8h <44212751+5aura8h@users.noreply.github.com>
2021-05-17 20:09:56 +0530

    using vars

--- tenant.tf
+++ tenant.tf
@@ -7,36 +7,34 @@ terraform {
   }
 }
 
-
 provider "aci" {
-
- username = "saukotha"
- password = "cisco!123"
- url = "https://10.48.168.2"
+ username = "${var.user_name}"
+ password = "${var.user_pwd}"
+ url = "${var.apic_url}"
 
 }
 
 resource "aci_tenant" "tenant" {
- name = "saukotha2222"
- description = "Using Terraform"
+ name = "${var.tenant_name}"
+ description = "${var.tenant_description}"
 }
 
 resource "aci_vrf" "vrf" {
+  tenant_dn = "${var.aci_tenant.tenant.id}"
+   name = "${var.vrf_name}"
+}
+
+resource "aci_bridge_domain" "bd100" {
   tenant_dn = "${aci_tenant.tenant.id}"
-  name = "vrf1"
+  description = "${var.bd200_description}"
+  name = "${var.bd100_name}"
+  relation_fv_rs_ctx = "${aci_vrf.vrf.id}"
+
 }
 
-resource "aci_bridge_domain" "bd" {
+resource "aci_bridge_domain" "bd200" {
   tenant_dn = "${aci_tenant.tenant.id}"
-  name = "bd1"
-  description = "using terraform"
+  description = "${var.bd200_description}"
+  name = "${var.bd200_name}"
+  relation_fv_rs_ctx = "${aci_vrf.vrf.id}"
 }
-
-resource "aci_subnet" "subnet" {
-  parent_dn = "${aci_bridge_domain.bd.id}"
-  description = "using terraform"
-  ip = "10.0.3.28/29"
-  scope = ["shared"]
-
-}
-
