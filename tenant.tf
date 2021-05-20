f2a9340aac5aad80061a06e82ae895a34a3abe98
5aura8h <44212751+5aura8h@users.noreply.github.com>
2021-05-21 00:03:19 +0530

    Update tenant.tf

--- tenant.tf
+++ tenant.tf
@@ -2,10 +2,10 @@ terraform {
   required_providers {
     aci = {
       source = "CiscoDevNet/aci"
-      version = "-> 0.5.4"
+      version = "-> 0.6.0"
     }
   }
-  required_version = ">= 0.14"
+ 
 }
 
 provider "aci" {
